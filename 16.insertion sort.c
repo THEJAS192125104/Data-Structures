4a49a92aaec3f6548c6f736ea48cf37a07d95d86
THEJAS192125104 <112466121+THEJAS192125104@users.noreply.github.com>
2023-05-16 08:16:50 +0530

    16.insertion sort.c

---
+++ 16.insertion sort.c
@@ -0,0 +1,40 @@
+#include <math.h>
+#include <stdio.h>
+void insertionSort(int arr[], int n)
+{
+    int i, key, j;
+    for (i = 1; i < n; i++)
+    {
+        key = arr[i];
+        j = i - 1;
+        while (j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+void printArray(int arr[], int n)
+{
+    int i;
+    for (i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+ 
+// Driver code
+int main()
+{
+    int arr[] = {12, 22,45,11,89,13,67,5, 6};
+    int n = sizeof(arr) / sizeof(arr[0]);
+ 
+    insertionSort(arr, n);
+    printf("Insertion Sort:\n");
+    printArray(arr, n);
+ 
+    return 0;
+}
+/*
+Insertion Sort:
+5 6 11 12 13 22 45 67 89
